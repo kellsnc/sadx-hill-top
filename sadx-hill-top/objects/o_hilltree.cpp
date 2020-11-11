2ae1ae285be35f9ac402406c6d3f26649c38f5ac
Kell <kellspam@outlook.fr>
2020-11-11 23:18:39 +0100

    Additional trees

--- sadx-hill-top/objects/o_hilltree.cpp
+++ sadx-hill-top/objects/o_hilltree.cpp
@@ -5,13 +5,14 @@ ModelInfo* ht_tree = nullptr;
 
 CollisionData HillTree_Col[] = {
 	{ 0, CollisionShape_Capsule2, 0x77, 0, 0, {0, 35.0f, 0}, 5.0f, 35.0f, 0, 0, 0, 0, 0 },
-	{ 0, CollisionShape_Capsule2, 0x77, 0, 0, {0, 25.0f, 0}, 5.0f, 25.0f, 0, 0, 0, 0, 0 }
+	{ 0, CollisionShape_Capsule2, 0x77, 0, 0, {0, 25.0f, 0}, 5.0f, 25.0f, 0, 0, 0, 0, 0 },
+	{ 0, CollisionShape_Capsule2, 0x77, 0, 0, {0, 20.0f, 0}, 1.0f, 20.0f, 0, 0, 0, 0, 0 }
 };
 
 void __cdecl HillTree_Display(ObjectMaster* obj) {
-	if (!MissedFrames) {
-		EntityData1* data = obj->Data1;
+	EntityData1* data = obj->Data1;
 
+	if (!MissedFrames && IsVisible(&data->Position, 30.0f * (data->Scale.x + data->Scale.y))) {
 		njSetTexture(LevelObjTexlists[1]);
 		njPushMatrixEx();
 		njTranslateEx(&data->Position);
@@ -43,26 +44,40 @@ void __cdecl HillTree(ObjectMaster* obj) {
 	EntityData1* data = obj->Data1;
 	
 	// Choose the model and collison based on set information
-	if (data->Scale.z == 1) {
-		data->Object = ht_tree->getmodel()->child->sibling;
-		Collision_Init(obj, &HillTree_Col[1], 1, 4);
-	}
-	else {
+	switch (static_cast<int>(data->Scale.z)) {
+	case 0:
 		data->Object = ht_tree->getmodel()->child;
 		Collision_Init(obj, &HillTree_Col[0], 1, 4);
+		break;
+	case 1:
+		data->Object = ht_tree->getmodel()->child->sibling;
+		Collision_Init(obj, &HillTree_Col[1], 1, 4);
+		break;
+	case 2:
+		data->Object = ht_tree->getmodel()->child->sibling->sibling;
+		Collision_Init(obj, &HillTree_Col[2], 1, 4);
+		break;
+	case 3:
+		data->Object = ht_tree->getmodel()->child->sibling->sibling->sibling;
+		break;
+	case 4:
+		data->Object = ht_tree->getmodel()->child->sibling->sibling->sibling->sibling;
+		break;
 	}
-
+	
 	// If the scale is null, set it to normal
 	if (data->Scale.x == 0) {
 		data->Scale.x = 1;
 	}
 
-	// Adjust position with scale
-	data->CollisionInfo->CollisionArray[0].center.y *= data->Scale.x + data->Scale.y;
+	if (data->CollisionInfo) {
+		// Adjust position with scale
+		data->CollisionInfo->CollisionArray[0].center.y *= data->Scale.x + data->Scale.y;
 
-	// Adjust collision size with scale
-	data->CollisionInfo->CollisionArray[0].a *= data->Scale.x;
-	data->CollisionInfo->CollisionArray[0].b *= data->Scale.x + data->Scale.y;
+		// Adjust collision size with scale
+		data->CollisionInfo->CollisionArray[0].a *= data->Scale.x;
+		data->CollisionInfo->CollisionArray[0].b *= data->Scale.x + data->Scale.y;
+	}
 	
 	obj->MainSub = HillTree_Main;
 	obj->DisplaySub = HillTree_Display;
