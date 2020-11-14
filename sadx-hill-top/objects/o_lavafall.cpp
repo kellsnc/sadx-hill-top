e660bde3106872ae6ba038468fef3724da187cd8
Kell <kellspam@outlook.fr>
2020-11-14 21:55:54 +0100

    New method for lava fall

--- sadx-hill-top/objects/o_lavafall.cpp
+++ sadx-hill-top/objects/o_lavafall.cpp
@@ -3,31 +3,19 @@
 
 ModelInfo* ht_lavafall = nullptr;
 
-static constexpr int lava_modelcount = 7;
+AnimationFile* ht_lavafall0_anm = nullptr;
+AnimationFile* ht_lavafall1_anm = nullptr;
+AnimationFile* ht_lavafall2_anm = nullptr;
+AnimationFile* ht_lavafall3_anm = nullptr;
 
-struct LavaFall_Worker {
-	NJS_OBJECT nodes[lava_modelcount];
-};
-
-void SetLavaMorph(LavaFall_Worker* wk, NJS_OBJECT* orig_object, Float scale, Float length, Float param3) {
-	for (int i = 0; i < lava_modelcount; ++i) {
-		
-		orig_object = orig_object->sibling;
-	}
-}
-
-void SetupLavaNodes(LavaFall_Worker* wk, NJS_OBJECT* object) {
-	for (int i = 0; i < lava_modelcount; ++i) {
-		memcpy(&wk->nodes[i], object, sizeof(NJS_OBJECT));
-		wk->nodes[i].evalflags &= 0xFFFFFFFD;
-		object = object->sibling;
-	}
-}
+NJS_ACTION LavaFall0_Action = { 0 };
+NJS_ACTION LavaFall1_Action = { 0 };
+NJS_ACTION LavaFall2_Action = { 0 };
+NJS_ACTION LavaFall3_Action = { 0 };
 
 void __cdecl LavaFall_Display(ObjectMaster* obj) {
 	EntityData1* data = obj->Data1;
-	LavaFall_Worker* wk = (LavaFall_Worker*)data->LoopData;
-
+	
 	if (!MissedFrames) {
 		njSetTexture(LevelObjTexlists[1]);
 		njPushMatrixEx();
@@ -37,7 +25,7 @@ void __cdecl LavaFall_Display(ObjectMaster* obj) {
 		njScalef(data->Scale.x);
 
 		SetupWorldMatrix();
-		DrawChunkObject(wk->nodes);
+		njCnkAction((NJS_ACTION*)data->LoopData, data->Scale.z);
 		
 		njPopMatrixEx();
 	}
@@ -46,9 +34,24 @@ void __cdecl LavaFall_Display(ObjectMaster* obj) {
 void __cdecl LavaFall_Main(ObjectMaster* obj) {
 	if (!ClipSetObject(obj)) {
 		EntityData1* data = obj->Data1;
-		LavaFall_Worker* wk = (LavaFall_Worker*)data->LoopData;
+		
+		// Smooth transition between frames
+		if (data->NextAction == 0) {
+			data->Scale.z += 0.5f;
 
-		SetLavaMorph(wk, data->Object->child, data->Scale.x, data->Scale.y, data->Scale.z);
+			if (data->Scale.z > 30.0f) {
+				data->Scale.z = 30.0f;
+				data->NextAction = 1;
+			}
+		}
+		else {
+			data->Scale.z -= 0.5f;
+
+			if (data->Scale.z < 0.0f) {
+				data->Scale.z = 0.0f;
+				data->NextAction = 0;
+			}
+		}
 
 		obj->DisplaySub(obj);
 	}
@@ -56,19 +59,44 @@ void __cdecl LavaFall_Main(ObjectMaster* obj) {
 
 void __cdecl LavaFall(ObjectMaster* obj) {
 	EntityData1* data = obj->Data1;
-	LavaFall_Worker* wk = new LavaFall_Worker;
 
+	data->Scale.x = 8;
 	data->Object = ht_lavafall->getmodel();
 
-	SetupLavaNodes(wk, data->Object->child);	
-	SetLavaMorph(wk, data->Object->child, data->Scale.x, data->Scale.y, data->Scale.z);
+	switch (static_cast<int>(data->Scale.y) % 4) {
+	case 0:
+	default:
+		data->LoopData = (Loop*)&LavaFall0_Action;
+		break;
+	}
 
-	data->LoopData = (Loop*)wk;
-
+	if (data->Scale.x == 0) {
+		data->Scale.x = 1;
+	}
+	
 	obj->MainSub = LavaFall_Main;
 	obj->DisplaySub = LavaFall_Display;
 }
 
 void LavaFall_LoadModel() {
 	LoadModel(&ht_lavafall, "ht_lavafall", ModelFormat_Chunk);
+
+	LoadAnimation(&ht_lavafall0_anm, "ht_lavafall0");
+	LoadAnimation(&ht_lavafall1_anm, "ht_lavafall1");
+	LoadAnimation(&ht_lavafall2_anm, "ht_lavafall2");
+	LoadAnimation(&ht_lavafall3_anm, "ht_lavafall3");
+
+	NJS_OBJECT* object = ht_lavafall->getmodel();
+
+	LavaFall0_Action.object = object;
+	LavaFall0_Action.motion = ht_lavafall0_anm->getmotion();
+
+	LavaFall1_Action.object = object;
+	LavaFall1_Action.motion = ht_lavafall1_anm->getmotion();
+
+	LavaFall2_Action.object = object;
+	LavaFall2_Action.motion = ht_lavafall2_anm->getmotion();
+
+	LavaFall3_Action.object = object;
+	LavaFall3_Action.motion = ht_lavafall3_anm->getmotion();
 }
