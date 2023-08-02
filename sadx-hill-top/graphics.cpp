2df408a20beee5e67126656390c05452abe53416
Kell <kellspam@outlook.fr>
2023-08-02 03:27:11 +0200

    Minor improvements to texture replacement

--- sadx-hill-top/graphics.cpp
+++ sadx-hill-top/graphics.cpp
@@ -1,82 +1,60 @@
 #include "pch.h"
-
-//  HD GUI Check:
-
-bool HD_GUI = false;
-
-void Check_HDGUI(const HelperFunctions& helperFunctions)
-{
-    HD_GUI = helperFunctions.Mods->find("sadx-hd-gui") != nullptr;
-}
-
-
-//  Stage Names:
+#include "SADXModLoader.h"
 
 #define ReplaceTex(pvm, pvr, folder, pngname, gbix, x, y) helperFunctions.ReplaceTexture(pvm, pvr, (std::string(path) + "\\textures\\" folder "\\" pngname ".png").c_str(), gbix, x, y);
+#define ReplacePVR(a, b) helperFunctions.ReplaceFile("system\\" a ".PVR", b);
 
-void Load_StageNames(const char* path, const HelperFunctions& helperFunctions)
+void Load_StageNames(const char* path, const HelperFunctions& helperFunctions, bool hd)
 {
-    if (!HD_GUI)
-    {
-        ReplaceTex("AVA_STNAM", "TX_ST10", "StageNames", "SD_HillTop", 365040, 128, 16);
-        ReplaceTex("AVA_STNAM_E", "TX_ST10_e", "StageNames", "SD_HillTop_E", 10000418, 128, 16);
-    }    
-
-    else
-    {
-        ReplaceTex("AVA_STNAM", "TX_ST10", "StageNames", "HD_HillTop", 365040, 128, 16);
-        ReplaceTex("AVA_STNAM_E", "TX_ST10_e", "StageNames", "HD_HillTop_E", 10000418, 128, 16);
-    }  
+	if (!hd)
+	{
+		ReplaceTex("AVA_STNAM", "TX_ST10", "StageNames", "SD_HillTop", 365040, 128, 16);
+		ReplaceTex("AVA_STNAM_E", "TX_ST10_e", "StageNames", "SD_HillTop_E", 10000418, 128, 16);
+	}
+	else
+	{
+		ReplaceTex("AVA_STNAM", "TX_ST10", "StageNames", "HD_HillTop", 365040, 128, 16);
+		ReplaceTex("AVA_STNAM_E", "TX_ST10_e", "StageNames", "HD_HillTop_E", 10000418, 128, 16);
+	}
 }
 
-
-//	TitleCards:
-
-#define ReplaceTitleCard_SD(a) _snprintf_s(pathbuf, LengthOfArray(pathbuf), "%s\\textures\\TitleCards_SD\\index.txt", path); \
-        helperFunctions.ReplaceFile("system\\" a ".PVR", pathbuf);
-
-#define ReplaceTitleCard_HD(a) _snprintf_s(pathbuf, LengthOfArray(pathbuf), "%s\\textures\\TitleCards_HD\\index.txt", path); \
-        helperFunctions.ReplaceFile("system\\" a ".PVR", pathbuf);
-
-void Load_TitleCards(const char* path, const HelperFunctions & helperFunctions)
+void Load_TitleCards(const char* path, const HelperFunctions& helperFunctions, bool hd)
 {
-	char pathbuf[MAX_PATH];
-
-    if (!HD_GUI)
+	std::string index = path;
+    if (!hd)
     {
-        ReplaceTitleCard_SD("S_STAGE07");
-        ReplaceTitleCard_SD("S_STAGE07_E");
-        ReplaceTitleCard_SD("K_STAGE03");
-        ReplaceTitleCard_SD("K_STAGE03_E");
-        ReplaceTitleCard_SD("E_STAGE04");
-        ReplaceTitleCard_SD("E_STAGE04_E");
-
-        // These _DC titlecards are meant to replace the ones DC Conversion uses when you are NOT using HD GUI.
-        ReplaceTitleCard_SD("S_STAGE07_DC");
-        ReplaceTitleCard_SD("S_STAGE07_E_DC");
-        ReplaceTitleCard_SD("K_STAGE03_DC");
-        ReplaceTitleCard_SD("K_STAGE03_E_DC");
-        ReplaceTitleCard_SD("E_STAGE04_DC");
-        ReplaceTitleCard_SD("E_STAGE04_E_DC");
+		index += "\\textures\\TitleCards_SD\\index.txt";
+		const char* sd_index = index.c_str();
+		ReplacePVR("S_STAGE07", sd_index);
+		ReplacePVR("S_STAGE07_E", sd_index);
+		ReplacePVR("K_STAGE03", sd_index);
+		ReplacePVR("K_STAGE03_E", sd_index);
+		ReplacePVR("E_STAGE04", sd_index);
+		ReplacePVR("E_STAGE04_E", sd_index);
+		ReplacePVR("S_STAGE07_DC", sd_index);
+		ReplacePVR("S_STAGE07_E_DC", sd_index);
+		ReplacePVR("K_STAGE03_DC", sd_index);
+		ReplacePVR("K_STAGE03_E_DC", sd_index);
+		ReplacePVR("E_STAGE04_DC", sd_index);
+		ReplacePVR("E_STAGE04_E_DC", sd_index);
     }
-    
     else
     {
-        ReplaceTitleCard_HD("S_STAGE07");
-        ReplaceTitleCard_HD("S_STAGE07_E");
-        ReplaceTitleCard_HD("K_STAGE03");
-        ReplaceTitleCard_HD("K_STAGE03_E");
-        ReplaceTitleCard_HD("E_STAGE04");
-        ReplaceTitleCard_HD("E_STAGE04_E");
+
+		index += "\\textures\\TitleCards_HD\\index.txt";
+		const char* hd_index = index.c_str();
+		ReplacePVR("S_STAGE07", hd_index);
+		ReplacePVR("S_STAGE07_E", hd_index);
+		ReplacePVR("K_STAGE03", hd_index);
+		ReplacePVR("K_STAGE03_E", hd_index);
+		ReplacePVR("E_STAGE04", hd_index);
+		ReplacePVR("E_STAGE04_E", hd_index);
     }
 }
-
-
-//  Init HD_GUI Check, StageNames and TitleCards:
 
 void Graphics_Init(const char* path, const HelperFunctions& helperFunctions)
 {
-    Check_HDGUI(helperFunctions);
-    Load_StageNames(path, helperFunctions);
-    Load_TitleCards(path, helperFunctions);
+	bool hd = helperFunctions.Mods->find("sadx-hd-gui") != nullptr;
+	Load_StageNames(path, helperFunctions, hd);
+    Load_TitleCards(path, helperFunctions, hd);
 }
